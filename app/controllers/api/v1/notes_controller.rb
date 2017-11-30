facd3cc7e90839fee3f00dca255c9fd68c1053d6
awongCM <awong.cm@gmail.com>
2017-11-30 20:36:53 +1100

    added the simple crud implementation in the api controller

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -4,5 +4,32 @@ module Api::V1
       @notes = Note.all
       render json: @notes
     end
+
+    def create
+      @note = Note.create(note_params)
+      render json: @note
+    end
+
+    def update
+      @note = Note.find(params[:id])
+      @note.update_attributes(note_params)
+      render json: @note
+    end
+
+    def destroy
+      @note = Note.find(params[:id])
+      if @note.destroy
+        head :no_content, status: :ok
+      else
+        render json: @note.errors, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def note_params
+      params.require(:note).permit(:title, :body)
+    end
+
   end
 end
