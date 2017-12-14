b6a9fbc257373458b5912a747d7110dc03d6e93f
awongCM <awong.cm@gmail.com>
2017-12-14 14:32:29 +1100

    added conditional routes for dev and prod envs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :api, path: '/' do
-    namespace :v1 do
-      resources :notes, constraints: {subdomain: 'api'}  
+  
+  if Rails.env.production?
+    namespace :api, path: '/' do
+      namespace :v1 do
+        resources :notes
+      end
+    end
+  else
+    namespace :api, path: '/' do
+      namespace :v1 do
+        resources :notes, constraints: {subdomain: 'api'}  
+      end
     end
   end
 end
