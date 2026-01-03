ee8d3138ecbe313e0df2213bf808656480ea89b1
awongCM <awong.cm@gmail.com>
2026-01-03 19:55:08 +1100

    added neon db service integration

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,5 +6,5 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 # NB: Render Postgres DB Free edition expired. Migrated to Supabase Postgres.
-# bundle exec rake db:migrate
-# bundle exec rake db:seed
+bundle exec rake db:migrate
+bundle exec rake db:seed
